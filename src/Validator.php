5da1dbb95a17fb30758060fd978df1da507fce62
jpmsilvamessias <joaopedr901673@gmail.com>
2025-09-20 15:48:25 -0300

    feat:validator complete

--- src/Validator.php
+++ src/Validator.php
@@ -9,7 +9,27 @@ class Validator{
     } 
 
     public static function validatePassword( User $user ): bool{
-        if($user->get)
+        if(strlen(trim($user->getPassword()))<8){
+           return false;
+        }
+        
+        if(!preg_match('/[A-Z]/', $user->getPassword())){
+            return false;
+        }
+
+        return true;
+        
       }
 
+      public static function createHash( User $user ): void{
+         $user->setPassword(password_hash($user->getPassword(), PASSWORD_DEFAULT));
+      }
+
+      public static function validateHash(string $senha, User $user) : bool{ 
+      if(!password_verify(senha,$user->getPassword())){
+          return false;
+         }
+        
+         return true;
+    }   
 }
