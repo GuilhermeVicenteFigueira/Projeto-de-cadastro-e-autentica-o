b1f019c9bfef8b6e52a1b410ef2aaa701f0e77f0
jpmsilvamessias <joaopedr901673@gmail.com>
2025-09-20 15:13:44 -0300

    feat: validator incomplete

--- src/Validator.php
+++ src/Validator.php
@@ -1,5 +1,15 @@
 <?php 
 
 class Validator{
-    
+    public static function validateEmail( User $user ): bool{
+          if(!filter_var($user->getEmail(), FILTER_VALIDATE_EMAIL)){
+            return false;
+          }
+           return true;
+    } 
+
+    public static function validatePassword( User $user ): bool{
+        if($user->get)
+      }
+
 }
