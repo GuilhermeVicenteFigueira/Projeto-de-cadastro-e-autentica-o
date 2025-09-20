354aeac3711af77f156c393b1cff255c6ecf0aea
GuilhermeVicenteFigueira <figueira.gui1@gmail.com>
2025-09-20 17:41:10 -0300

    Inicio do User Manager

--- src/UserManager.php
+++ src/UserManager.php
@@ -1,5 +1,82 @@
 <?php 
 
+require_once 'Validator.php';
+
 class UserManager{
-    
+
+    private User $user;
+    private array $users = [];
+
+    public function __construct(User $user, array $users) 
+        {
+          $this->user = $user;
+          $this->users =$users;
+        }
+     
+    public function createUser(User $user): bool
+    {
+        if(!Validator->validateEmail($user))
+        {
+            echo "Email invalido";
+            return false;
+        }
+
+        if(!Validator->validatePassword($user))
+        {
+            echo "Senha invalida";
+            return false;
+        }
+
+        if(!UserManager->hasSameEmail($user))
+        {
+            echo "Email Invalido";
+            return false;
+        }
+
+        Validator->createHash($user);
+
+        $this->users[] = [
+            'Id'=>$user->getId(),
+            'Nome'=>$user->getNome(),
+            'Email'=>$user->setEmail(),
+            'Senha'=>$user->getPassword(),
+        ];
+        
+    }
+
+    public function hasSameEmail($user): bool
+    {
+        foreach($this->user as $index){
+            if($user['Email']===$user)
+                return false;
+        }
+            return true;
+    }
+
+
+
+    public function loginUser($user): void{
+
+        if(!Validator->validateEmail($user))
+        {
+            echo "Email invalido";
+        }
+
+        if(!Validator->validatePassword($user))
+        {
+            echo "Senha invalida";
+        }
+
+        if(Validator->validateHash($user))
+        {
+            echo"Logado com sucesso";
+        }
+
+        echo"login não encontrado"; 
+    }
+
+    //Reset de Senha
+    //Permitir atualizar a senha de um usuário existente.
+    //Aplicar novamente as regras de senha forte.
+    //Substituir pela nova senha com password_hash.
 }
--- src/Validator.php
+++ src/Validator.php
@@ -1,35 +1,40 @@
 <?php 
 
 class Validator{
-    public static function validateEmail( User $user ): bool{
-          if(!filter_var($user->getEmail(), FILTER_VALIDATE_EMAIL)){
-            return false;
-          }
-           return true;
+  public static function validateEmail( User $user ): bool{
+      if(!filter_var($user->getEmail(), FILTER_VALIDATE_EMAIL))
+      {
+        return false;
+      }
+        return true;
+
     } 
 
-    public static function validatePassword( User $user ): bool{
-        if(strlen(trim($user->getPassword()))<8){
-           return false;
-        }
+  public static function validatePassword( User $user ): bool{
+    if(strlen(trim($user->getPassword()))<8)
+      {
+        return false;
+      }
         
-        if(!preg_match('/[A-Z]/', $user->getPassword())){
-            return false;
-        }
-
-        return true;
-        
+    if(!preg_match('/[A-Z]/', $user->getPassword()))
+      {
+        return false;
       }
 
-      public static function createHash( User $user ): void{
-         $user->setPassword(password_hash($user->getPassword(), PASSWORD_DEFAULT));
-      }
+      return true;
+     
+    }
 
-      public static function validateHash(string $senha, User $user) : bool{ 
-      if(!password_verify(senha,$user->getPassword())){
-          return false;
-         }
+  public static function createHash( User $user ): void{
+      $user->setPassword(password_hash($user->getPassword(), PASSWORD_DEFAULT));
+    }
+
+  public static function validateHash(string $senha, User $user) : bool{ 
+    if(!password_verify($senha,$user->getPassword()))
+      {
+        return false;
+      }
         
-         return true;
-    }   
+      return true;
+    }
 }
